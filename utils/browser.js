0c906360778d36012926070869c8ceff06069990
jericho <jericho.sosas23@gmail.com>
2026-02-03 01:25:16 +0800

    new

--- utils/browser.js
+++ utils/browser.js
@@ -2,7 +2,13 @@ import { chromium } from 'playwright';
 
 export async function launchBrowser() {
   return chromium.launch({
-    headless: true,  // always use headless in server
-    args: ['--no-sandbox', '--disable-setuid-sandbox']  // required for Linux servers
+    headless: true, // always headless on server
+    args: [
+      '--no-sandbox',          // required for Linux containers
+      '--disable-setuid-sandbox',
+      '--disable-dev-shm-usage',
+      '--disable-gpu',
+      '--single-process'
+    ]
   });
 }
